0d43b701103295d3946efd29b46a359f4078aa7a
Eduardo de Oliveira Ellwanger <eduardo.ellwanger@pucpr.edu.br>
2026-05-04 19:31:14 -0300

    fix: paths das paginas web

--- app/views.py
+++ app/views.py
@@ -5,3 +5,15 @@ from flask import render_template
 @app.route('/')
 def cadastro():
     return render_template("cadastro/cadastro.html")
+
+@app.route('/index')
+def index():
+    return render_template("index/index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login/login.html")
+
+@app.route('/login2')
+def login2():
+    return render_template("login/login2.html")
